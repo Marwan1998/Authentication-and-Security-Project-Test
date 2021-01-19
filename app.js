59f5a958dc8f6f792966602d512484d3c733aade
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-01-19 22:08:24 +0200

    this is level 2 security

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const say = console.log;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+const secret = "AVeryveryBigPaSSWord.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+
+  const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+  }).save(function(err){
+    if(!err){
+      res.render("secrets");
+    }else{
+      res.send(err);
+    }
+  });
+
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if(!err){
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }else{
+      res.send(err);
+    }
+  });
+
+});
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  say("server is running...");
+});
