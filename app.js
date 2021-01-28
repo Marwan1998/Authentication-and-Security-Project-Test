aeba836e19f5828f19b739b46c9dd8b70bf453c8
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-01-28 23:53:33 +0200

    This is Level 6 security, it includes OAuth, authentication va google to authenticate the users and get thire data from google account.

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const say = console.log;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var findOrCreate = require('mongoose-findorcreate')
+
+
 const app = express();
 
 
@@ -34,19 +38,53 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req, res) {
   res.render("login");
@@ -62,12 +100,41 @@ app.get("/logout", function(req, res) {
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne:null}}, function(err, foundUsers){
+    if(err){
+      say(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   } else {
     say("Not Authennticated! err" + req.isAuthenticated());
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      say(err)
+    }else{
+      foundUser.secret = submittedSecret;
+      foundUser.save(function(){
+        res.redirect("secrets");
+      });
+    }
+  });
+
+  say(req.user);
 });
 
 app.post("/register", function(req, res) {
@@ -101,6 +168,7 @@ app.post("/login", function(req, res) {
     }
   });
 });
+
 
 
 
